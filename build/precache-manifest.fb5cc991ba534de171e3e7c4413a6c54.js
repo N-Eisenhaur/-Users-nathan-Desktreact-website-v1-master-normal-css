00e24d07610535364fbac89aef415d1be1019770
codercypt <56701242+codercypt@users.noreply.github.com>
2021-09-07 18:48:48 -0300

    1st

---
+++ build/precache-manifest.fb5cc991ba534de171e3e7c4413a6c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedb20c79fc1c7684d6632d497d39557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4774decd73fb92a5cae7",
+    "url": "/static/css/main.c44cab7b.chunk.css"
+  },
+  {
+    "revision": "ed366eb663530ee4b20c",
+    "url": "/static/js/2.60841393.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.60841393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4774decd73fb92a5cae7",
+    "url": "/static/js/main.2a328d3c.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
---
+++ src/components/Navbar.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);//sets click function values
+  const closeMobileMenu = () => setClick(false);//click makes false closes
+
+  const showButton = () => {//displays button based on screen size
+    if (window.innerWidth <= 960) {//<= means  less than or equal to
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);//if screen resized then showbutton will be active
+
+  //there is font that is needed to keep things straight in the hamburger menu in index.html
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            TRVL
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
+        </div>
+      </nav>
+    </>//&& returns/renders whats after also means if true
+  );
+}
+
+export default Navbar;
